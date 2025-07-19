6e669e287a8639d784361dd6631ec17002d596a1
wowitsraxel <wowitsraxel@gmail.com>
2025-07-19 02:00:51 -0600

    added chisel advanced item

--- src/main/java/net/saylemm/tutorialmod/item/ModItems.java
+++ src/main/java/net/saylemm/tutorialmod/item/ModItems.java
@@ -7,10 +7,14 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.saylemm.tutorialmod.TutorialMod;
+import net.saylemm.tutorialmod.item.custom.ChiselItem;
 
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
